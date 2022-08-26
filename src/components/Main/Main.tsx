6576204e23c0ecba5a68d193056e1bbe919dea38
dmitr_zubakin <dmitr_zubakin@sourcemates.com>
2022-08-26 13:38:08 +0300

    feature: add constants

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {requests} from '../../Request';
 import axios from "axios";
+import {IMAGE_URL} from "../../constans/contstans";
 
  interface MainProps {
     adult:             boolean;
@@ -34,7 +35,7 @@ useEffect(()=>{
         <div className='w-full h-[550px] text-white'>
             <div className='w-full h-full'>
                 <div className='absolute w-full h-[550px] bg-gradient-to-r from-black' ></div>
-            <img className='w-full h-full object-cover' src={`https://image.tmdb.org/t/p/original/${movie?.poster_path}`} alt={movie?.title}/>
+            <img className='w-full h-full object-cover' src={`${IMAGE_URL}/${movie?.poster_path}`} alt={movie?.title}/>
             </div>
         <div>
     <button>Play</button>
