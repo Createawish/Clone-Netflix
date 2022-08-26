94d231ea6688c3276ba8c8578841deeb38847a8f
Createawish <bezlyudovalera1996@gmail.com>
2022-08-27 02:10:37 +0300

    feature: add scroll row

--- src/components/Row/Row.tsx
+++ src/components/Row/Row.tsx
@@ -1,26 +1,43 @@
 import React from 'react';
 import {UseRow} from "./useRow";
-import {IMAGE_SCROLL_URL} from "../../constants/contstants";
+import {Movie} from "../Movie/Movie";
+import {MdChevronLeft,MdChevronRight} from 'react-icons/md';
 
 interface IRowProps {
     title:string,
-    fetchURL:string
+    fetchURL:string,
+    rowId:string
 }
 
-export const Row = ({title,fetchURL}:IRowProps):JSX.Element => {
-    const movies = UseRow(fetchURL)
+export const Row = ({title,fetchURL,rowId}:IRowProps):JSX.Element => {
+    const movies = UseRow(fetchURL);
+
+    const sliderLeft = () => {
+        let slider = document.getElementById('slider' + rowId);
+        // @ts-ignore
+        slider.scrollLeft = slider.scrollLeft - 500;
+    }
+    const sliderRight = () => {
+        let slider = document.getElementById('slider' + rowId);
+        // @ts-ignore
+        slider.scrollLeft = slider.scrollLeft + 500;
+    }
 
     return (
         <>
         <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
-            <div className='relative  flex items-center'>
-                <div id={'slider'}>
+            <div className='relative  flex items-center group'>
+                <MdChevronLeft
+                    onClick={sliderLeft}
+                    className='bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' size={40}/>
+                <div id={'slider' + rowId} className='w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative'>
                     {movies.map((item, id) =>(
-                        <div className='w-[160px] sm:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2' key={id}>
-                            <img src={`${IMAGE_SCROLL_URL}${item?.backdrop_path}`} alt={item?.title}/>
-                        </div>
+                        <Movie key={id} item={item}/>
                     ) )}
                 </div>
+                <MdChevronRight
+                    onClick = {sliderRight}
+                    className='bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block' size={40}/>
             </div>
         </>
     );
